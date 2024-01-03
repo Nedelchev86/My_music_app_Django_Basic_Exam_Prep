cce612b5d9cc630ca019054eef062856f7cafccb
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-01-03 20:14:30 +0200

    Added home page with registered user - Show Albums

--- my_music_app2/albums/urls.py
+++ my_music_app2/albums/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from my_music_app2.albums.views import AlbumDetails
+from my_music_app2.albums.views import AlbumDetails, AddAlbum
 
 urlpatterns = [
     path('<int:pk>/details/', AlbumDetails.as_view(), name='album_details'),
+    path('album/add/', AddAlbum.as_view(), name='add_album'),
 ]
--- my_music_app2/albums/views.py
+++ my_music_app2/albums/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, CreateView
 
 from my_music_app2.albums.models import Album
 
@@ -10,3 +10,37 @@ from my_music_app2.albums.models import Album
 class AlbumDetails(DetailView):
     model = Album
     template_name = "album/album-details.html"
+
+
+
+class AddAlbum(CreateView):
+    model = Album
+    fields = ["album_name", "artist", "genre", "description", "image_url", "price"]
+
+    template_name = "album/album-add.html"
+
+    labels = {
+        "album_name": "Album Name",
+        "artist": "Artist",
+        "genre": "Genre",
+        "description": "Description",
+        "image_url": "Image URL",
+        "price": "Price",
+    }
+    def get_form(self, form_class=None):
+        if form_class is None:
+            form_class = self.get_form_class()
+
+        form = super().get_form(form_class)
+        form.fields['album_name'].widget.attrs ={'placeholder': 'Album Name'}
+        form.fields['artist'].widget.attrs ={'placeholder': 'Artist Name'}
+        form.fields['description'].widget.attrs ={'placeholder': 'Description'}
+        form.fields['image_url'].widget.attrs ={'placeholder': 'Image Url'}
+        form.fields['price'].widget.attrs ={'placeholder': 'Price'}
+
+        for field_name, label in self.labels.items():
+            form.fields[field_name].label = label
+        return form
+
+
+
