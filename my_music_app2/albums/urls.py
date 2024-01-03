761c752165f688f17356b05b2c184969c774be4f
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-01-03 19:32:37 +0200

    Added home page with registered user - Show Albums

--- my_music_app2/albums/urls.py
+++ my_music_app2/albums/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
-urlpatterns = [
+from my_music_app2.albums.views import AlbumDetails
 
+urlpatterns = [
+    path('<int:pk>/details/', AlbumDetails.as_view(), name='album_details'),
 ]
--- my_music_app2/albums/views.py
+++ my_music_app2/albums/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+
+from my_music_app2.albums.models import Album
+
 
 # Create your views here.
+
+
+class AlbumDetails(DetailView):
+    model = Album
+    template_name = "album/album-details.html"
